6274d7b3cb537d9d96181857f98fef9630aef7e1
omarebehery-dotcom <omar.ebehery@gmail.com>
2026-07-15 16:12:51 +0300

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,14 @@
+var map = L.map('map').setView([0, 0], 2);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((pos) => {
+        const lat = pos.coords.latitude;
+        const lng = pos.coords.longitude;
+        map.setView([lat, lng], 15);
+        L.marker([lat, lng]).addTo(map).bindPopup("أنت هنا!").openPopup();
+    });
+}
