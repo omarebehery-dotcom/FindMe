5fe837898219a0b459bb8825e3f9f331d3537409
omarebehery-dotcom <omar.ebehery@gmail.com>
2026-07-18 13:56:21 +0300

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -1,1 +1,45 @@
+// traffic.js
 
+let trafficLayer = null;
+
+let trafficEnabled = false;
+
+function toggleTraffic() {
+
+    if (trafficEnabled) {
+
+        if (trafficLayer) {
+
+            map.removeLayer(trafficLayer);
+
+        }
+
+        trafficEnabled = false;
+
+        alert("Traffic OFF");
+
+    } else {
+
+        trafficLayer = L.tileLayer(
+
+            "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+
+            {
+
+                maxZoom: 19,
+
+                opacity: 0.6
+
+            }
+
+        );
+
+        trafficLayer.addTo(map);
+
+        trafficEnabled = true;
+
+        alert("Traffic ON");
+
+    }
+
+}
