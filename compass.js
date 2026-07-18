7ff3f42f53535ee5367ba55bf87ec535adf1d1fd
omarebehery-dotcom <omar.ebehery@gmail.com>
2026-07-18 13:53:50 +0300

    Update compass.js

--- compass.js
+++ compass.js
@@ -1,1 +1,89 @@
+// compass.js
 
+let compassArrow = document.getElementById("compassArrow");
+let compassText = document.getElementById("compassText");
+
+function rotateCompass(heading) {
+
+    if (compassArrow) {
+        compassArrow.style.transform = `rotate(${-heading}deg)`;
+    }
+
+    if (compassText) {
+        compassText.innerHTML = Math.round(heading) + "°";
+    }
+
+}
+
+function startCompass() {
+
+    if (!window.DeviceOrientationEvent) {
+
+        if (compassText) {
+            compassText.innerHTML = "Compass Not Supported";
+        }
+
+        return;
+    }
+
+    // iPhone
+    if (typeof DeviceOrientationEvent.requestPermission === "function") {
+
+        DeviceOrientationEvent.requestPermission()
+
+            .then(permission => {
+
+                if (permission === "granted") {
+
+                    window.addEventListener(
+                        "deviceorientation",
+                        compassHandler,
+                        true
+                    );
+
+                }
+
+            })
+
+            .catch(console.error);
+
+    }
+
+    // Android
+    else {
+
+        window.addEventListener(
+            "deviceorientationabsolute",
+            compassHandler,
+            true
+        );
+
+        window.addEventListener(
+            "deviceorientation",
+            compassHandler,
+            true
+        );
+
+    }
+
+}
+
+function compassHandler(event) {
+
+    let heading;
+
+    if (event.webkitCompassHeading !== undefined) {
+
+        heading = event.webkitCompassHeading;
+
+    } else {
+
+        heading = 360 - event.alpha;
+
+    }
+
+    rotateCompass(heading);
+
+}
+
+startCompass();
